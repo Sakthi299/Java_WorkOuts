77ead07e60b40e46dccb39625c1f2a68d462eee9
Sakthi Namasivayam <60822530+Sakthi299@users.noreply.github.com>
2020-09-02 17:24:07 +0530

    Local_variables

---
+++ Java_Basics & Swings/LocalVariables.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+public class LocalVariables {
+    public void disp()
+    {
+        int k=100;
+        System.out.println("k="+k);
+    }
+    public static void main(String[] args)
+    {
+        LocalVariables obj = new LocalVariables();
+        obj.disp();
+    }
+    
+    
+}
